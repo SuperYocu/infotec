cbb3add28338ff0c80ba6035b7b4ad5c889a51a6
SuperYocu <superyocu@gmail.com>
2026-01-31 03:59:19 +0000

    Agregada vista de Ponente

--- src/app/Http/Controllers/PonenteVistaController.php
+++ src/app/Http/Controllers/PonenteVistaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Ponente;
 
 class PonenteVistaController extends Controller
@@ -12,3 +13,30 @@ class PonenteVistaController extends Controller
         return view('ponentes.vista',compact('ponentes'));
     }
 }
+
+public function store(Request $request){
+    $validator =Validator::make($request->all(),[
+        'nombre'=>'required',
+        'biografia'=>'nullable',
+        'especialidad'=>'nullable',
+    ]);
+    if($validator->fails()){
+        return redirect()->route('ponentes.vista')->with('error','Faltan datos obligatorios');
+    }
+
+    Ponente::create($request->all());
+
+    return redirect()->route('ponentes.vista')->with('success','Ponente agregado correctamente');
+}
+
+public function destroy($id){
+    $ponente=Ponente::find($id);
+
+    if($ponente){
+        $ponente->delete();
+        return redirect()->route('ponentes.vista')->with('success','Ponente eliminado correctamente');
+    }
+
+    return redirect()->route('ponentes.vista')->with('error','Ponente no encontrado');
+}
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,4 +15,6 @@ class PonenteVistaController extends Controller
 }
 
 Route::get('/ponentes-vista',[PonenteVistaController::class,'index'])->name('ponentes.vista');
+Route::post('/ponentes-vista',[PonenteVistaController::class,'store'])->name('ponentes.store');
+Route::delete('/ponentes-vista/{id}', [PonenteVistaController::class, 'destroy'])->name('ponentes.destroy');
 
